3700e3bbb8243dafee3b26cdcc28abd4d951f2f8
personjc <personjc@gmail.com>
2019-11-29 22:56:33 -0500

    adding update and delete methods to service

--- src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
+++ src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository("fakeDao")
@@ -23,5 +24,37 @@ public class FakePersonDataAccessService implements PersonDao {
     public List<Person> getAllPeople(){
         return DB;
     }
+
+    @Override
+    public Optional<Person> selectPersonById(UUID id) {
+        return DB.stream()
+                .filter(person -> person.getId().equals(id)).findFirst();
+    }
+
+    @Override
+    public int deletePersonById(UUID id) {
+        Optional<Person> person = selectPersonById(id);
+
+        if(person.isEmpty()){
+            return 0;
+        } else {
+            DB.remove(person.get());
+            return 1;
+        }
+    }
+
+    @Override
+    public int updatePersonById(UUID id, Person person) {
+        Optional<Person> potentialPerson = selectPersonById(id);
+
+        if(potentialPerson.isEmpty()){
+            return 0;
+        } else {
+            int index = DB.indexOf(potentialPerson.get());
+            DB.set(index, person);
+            return 1;
+        }
+    }
+
 
 }
